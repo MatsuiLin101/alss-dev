63c6fe0f9d41942550afd9e4f248f6a84824b004
wups101 <35325179+wups101@users.noreply.github.com>
2018-05-08 14:32:54 +0800

    test templat and update basefarmer models

--- src/basefarmers/admin.py
+++ src/basefarmers/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
-from .models import MarketType, IncomeRangeCode, AnnualIncome, BaseFarmer, AddressMatch, Lack
+from .models import (
+    MarketType,
+    IncomeRangeCode,
+    AnnualIncome,
+    BaseFarmer,
+    AddressMatch,
+    Lack,
+    Phone,
+    FarmerLandArea,
+    FarmerLandType,
+    FarmerProductionType,
+    FarmRelatedBusiness,
+    Business,
+    Management,
+    ManagementType,
+)
 
 admin.site.register(MarketType)
 admin.site.register(IncomeRangeCode)
@@ -7,3 +22,11 @@ admin.site.register(AnnualIncome)
 admin.site.register(BaseFarmer)
 admin.site.register(AddressMatch)
 admin.site.register(Lack)
+admin.site.register(Phone)
+admin.site.register(FarmerLandArea)
+admin.site.register(FarmerLandType)
+admin.site.register(FarmerProductionType)
+admin.site.register(FarmRelatedBusiness)
+admin.site.register(Business)
+admin.site.register(ManagementType)
+admin.site.register(Management)
--- src/basefarmers/models.py
+++ src/basefarmers/models.py
@@ -10,6 +10,7 @@ from django.db.models import (
     CharField,
     DateTimeField,
     ForeignKey,
+    OneToOneField,
     ManyToManyField,
     IntegerField,
     BooleanField,
@@ -17,182 +18,6 @@ from django.db.models import (
     DateField,
     Q
 )
-'''
-class AbstractProduct(Model):
-    name = CharField(max_length=50, verbose_name=_('Name'))
-    code = CharField(max_length=50, verbose_name=_('Code'))
-    config = ForeignKey('configs.Config', null=True, on_delete=SET_NULL, verbose_name=_('Config'))
-    type = ForeignKey('configs.Type', null=True, on_delete=SET_NULL, verbose_name=_('Type'))
-    parent = ForeignKey('self', null=True, blank=True, on_delete=SET_NULL, verbose_name=_('Parent'))
-    track_item = BooleanField(default=True, verbose_name=_('Track Item'))
-    update_time = DateTimeField(auto_now=True, auto_now_add=False, null=True, blank=True, verbose_name=_('Updated'))
-
-    objects = InheritanceManager()
-
-    class Meta:
-        verbose_name = _('Abstract Product')
-        verbose_name_plural = _('Abstract Products')
-
-    def __str__(self):
-        return str(self.name)
-
-    def __unicode__(self):
-        return str(self.name)
-
-    def children(self):
-        return AbstractProduct.objects.filter(parent=self).select_subclasses()
-
-    def children_all(self):
-        return AbstractProduct.objects.filter(
-            Q(parent=self) | Q(parent__parent=self) | Q(parent__parent__parent=self)
-        ).select_subclasses()
-
-    @property
-    def has_child(self):
-        if self.children().count() > 0:
-            return True
-        else:
-            return False
-
-    def types(self):
-        if self.children():
-            type_dics = self.children_all().values('type').distinct()
-            type_ids = [d['type'] for d in type_dics]
-            return Type.objects.filter(id__in=type_ids)
-        elif self.type:
-            return Type.objects.filter(id=self.type.id)
-        else:
-            return []
-
-    def sources(self):
-        return Source.objects.filter(configs__id__exact=self.config.id).filter(type=self.type)
-
-    def unit(self):
-        return self.config.unit
-
-
-class Config(Model):
-    name = CharField(max_length=50, unique=True, verbose_name=_('Name'))
-    code = CharField(max_length=50, unique=True, null=True, verbose_name=_('Code'))
-    charts = ManyToManyField('configs.Chart', blank=True, verbose_name=_('Chart'))
-    unit = ForeignKey('configs.Unit', null=True, blank=True, verbose_name=_('Unit'))
-    type_level = IntegerField(choices=[(1, 1), (2, 2)], default=1, verbose_name=_('Type Level'))
-    update_time = DateTimeField(auto_now=True, auto_now_add=False, null=True, blank=True, verbose_name=_('Updated'))
-
-    class Meta:
-        verbose_name = _('Config')
-        verbose_name_plural = _('Configs')
-
-    def __str__(self):
-        return str(self.name)
-
-    def __unicode__(self):
-        return str(self.name)
-
-    def products(self):
-        # Use select_subclasses() to return subclass instance
-        return AbstractProduct.objects.filter(config=self).select_subclasses()
-
-    def first_level_products(self):
-        # Use select_subclasses() to return subclass instance
-        return AbstractProduct.objects.filter(config=self).filter(parent=None).select_subclasses()
-
-    def types(self):
-        products_qs = self.products().values('type').distinct()
-        types_ids = [p['type'] for p in products_qs]
-        types_qs = Type.objects.filter(id__in=types_ids)
-        return types_qs
-
-
-class SourceQuerySet(QuerySet):
-    """ for case like Source.objects.filter(config=config).filter_by_name(name) """
-    def filter_by_name(self, name):
-        if not isinstance(name, str):
-            raise TypeError
-        name = name.replace('台', '臺')
-        return self.filter(name=name)
-
-
-class Source(Model):
-    name = CharField(max_length=50, verbose_name=_('Name'))
-    code = CharField(max_length=50, null=True, verbose_name=_('Code'))
-    configs = ManyToManyField('configs.Config', verbose_name=_('Config'))
-    type = ForeignKey('configs.Type', null=True, on_delete=SET_NULL, verbose_name=_('Type'))
-    update_time = DateTimeField(auto_now=True, auto_now_add=False, null=True, blank=True, verbose_name=_('Updated'))
-
-    objects = SourceQuerySet.as_manager()
-
-    class Meta:
-        verbose_name = _('Source')
-        verbose_name_plural = _('Sources')
-
-    def __str__(self):
-        flat = self.configs_flat
-        return str(self.name) + '(%s-%s)' % (flat, self.type.name)
-
-    def __unicode__(self):
-        flat = self.configs_flat
-        return str(self.name) + '(%s-%s)' % (flat, self.type.name)
-
-    @property
-    def simple_name(self):
-        return self.name.replace('臺', '台')
-
-    @property
-    def configs_flat(self):
-        return ','.join(config.name for config in self.configs.all())
-
-
-class Type(Model):
-    name = CharField(max_length=50, unique=True, verbose_name=_('Name'))
-    update_time = DateTimeField(auto_now=True, auto_now_add=False, null=True, blank=True, verbose_name=_('Updated'))
-
-    class Meta:
-        verbose_name = _('Type')
-        verbose_name_plural = _('Types')
-
-    def __str__(self):
-        return str(self.name)
-
-    def __unicode__(self):
-        return str(self.name)
-
-    def sources(self):
-        return Source.objects.filter(type=self)
-
-
-class Unit(Model):
-    name = CharField(max_length=50, unique=True, verbose_name=_('Unit'))
-    update_time = DateTimeField(auto_now=True, auto_now_add=False, null=True, blank=True, verbose_name=_('Updated'))
-
-    class Meta:
-        verbose_name = _('Unit')
-        verbose_name_plural = _('Units')
-
-    def __str__(self):
-        return str(self.name)
-
-    def __unicode__(self):
-        return str(self.name)
-
-
-class Chart(Model):
-    name = CharField(max_length=120, unique=True, verbose_name=_('Name'))
-    code = CharField(max_length=50, unique=True, null=True, verbose_name=_('Code'))
-    ajax = CharField(max_length=120, null=True, blank=True, verbose_name=_('Ajax'))
-    static = CharField(max_length=240, default='mychart.js', verbose_name=_('Static'))
-    update_time = DateTimeField(auto_now=True, auto_now_add=False, null=True, blank=True, verbose_name=_('Updated'))
-
-    class Meta:
-        verbose_name = _('Chart')
-        verbose_name_plural = _('Charts')
-
-    def __str__(self):
-        return str(self.name)
-
-    def __unicode__(self):
-        return str(self.name)
-'''
 
 
 class BaseFarmer(Model):
@@ -200,7 +25,7 @@ class BaseFarmer(Model):
     read_only: Keep original data(read_only=True). Modify data(read_only=False).
     """
     farmer_id = CharField(max_length=12, verbose_name=_('Farmer Id'))
-    farmer_name = CharField(max_length=10, verbose_name=_('Farmer Name'))
+    farmer_name = CharField(null=True, blank=True, max_length=10, verbose_name=_('Farmer Name'))
     total_pages = IntegerField( verbose_name=_('Total Pages'))
     page = IntegerField( verbose_name=_('Page'))
     ori_class = IntegerField(null=True, blank=True, verbose_name=_('Original Class'))
@@ -213,7 +38,7 @@ class BaseFarmer(Model):
     read_only = BooleanField(default=True, verbose_name=_('Read Only'))
     update_time = DateTimeField(auto_now=True, auto_now_add=False, null=True, blank=True, verbose_name=_('Updated'))
 
-    annual_income = ManyToManyField('basefarmers.AnnualIncome', blank=True, verbose_name=_('Annual Income'))
+    #annual_income = ManyToManyField('basefarmers.AnnualIncome', blank=True, verbose_name=_('Annual Income'))
     #address_match = ForeignKey('basefarmers.AddressMatch', related_name='basefarmer', null=True, blank=True, verbose_name=_('Is Address Match'))
     lack = ManyToManyField('basefarmers.Lack', blank=True, verbose_name=_('Lack'))
 
@@ -228,6 +53,200 @@ class BaseFarmer(Model):
         return self.farmer_id
 
 
+# class Loss(Model):
+#     code = IntegerField(verbose_name=_('Loss Code'))
+#     name = CharField(max_length=10, null=True, blank=True, verbose_name=_('Loss Name'))
+#     type = CharField(max_length=20, null=True, blank=True, verbose_name=_('Loss Type'))
+#     update_time = DateTimeField(auto_now=True, auto_now_add=False, null=True, blank=True, verbose_name=_('Updated'))
+#
+#     class Meta:
+#         verbose_name = _('Loss')
+#         verbose_name_plural = _('Loss')
+#
+#     def __str__(self):
+#         return str(self.name)
+#
+#     def __unicode__(self):
+#         return str(self.name)
+#
+#
+# class Unit(Model):
+#     code = IntegerField(verbose_name=_('Unit Code'))
+#     name = CharField(max_length=10, null=True, blank=True, verbose_name=_('Unit Name'))
+#     type = CharField(max_length=20, null=True, blank=True, verbose_name=_('Unit Type'))
+#     update_time = DateTimeField(auto_now=True, auto_now_add=False, null=True, blank=True, verbose_name=_('Updated'))
+#
+#     class Meta:
+#         verbose_name = _('Unit')
+#         verbose_name_plural = _('Unit')
+#
+#     def __str__(self):
+#         return str(self.name)
+#
+#     def __unicode__(self):
+#         return str(self.name)
+#
+#
+# class ProductCode(Model):
+#     code = IntegerField(verbose_name=_('Product Code'))
+#     name = CharField(max_length=50, null=True, blank=True, verbose_name=_('Product Code Name'))
+#     type = CharField(max_length=20, null=True, blank=True, verbose_name=_('Product Type'))
+#     update_time = DateTimeField(auto_now=True, auto_now_add=False, null=True, blank=True, verbose_name=_('Updated'))
+#
+#     class Meta:
+#         verbose_name = _('ProductCode')
+#         verbose_name_plural = _('ProductCode')
+#
+#     def __str__(self):
+#         return str(self.name)
+#
+#     def __unicode__(self):
+#         return str(self.name)
+#
+#
+# class Contract(Model):
+#     code = IntegerField(verbose_name=_('Contract Code'))
+#     name = CharField(max_length=10, null=True, blank=True, verbose_name=_('Contract Name'))
+#     update_time = DateTimeField(auto_now=True, auto_now_add=False, null=True, blank=True, verbose_name=_('Updated'))
+#
+#     class Meta:
+#         verbose_name = _('AnimalUnit')
+#         verbose_name_plural = _('AnimalUnit')
+#
+#     def __str__(self):
+#         return str(self.name)
+#
+#     def __unicode__(self):
+#         return str(self.name)
+
+
+class ManagementType(Model):
+    name = CharField(max_length=50, null=True, blank=True, verbose_name=_('Management Type Name'))
+    update_time = DateTimeField(auto_now=True, auto_now_add=False, null=True, blank=True, verbose_name=_('Updated'))
+
+    class Meta:
+        verbose_name = _('ManagementType')
+        verbose_name_plural = _('ManagementType')
+
+    def __str__(self):
+        return str(self.name)
+
+    def __unicode__(self):
+        return str(self.name)
+
+
+class Management(Model):
+    basefarmer = OneToOneField('basefarmers.BaseFarmer', related_name='management', verbose_name=_('BaseFarmer'))
+    management_type = ManyToManyField('basefarmers.FarmRelatedBusiness', related_name='management_type',
+                                            verbose_name=_('Management Type'))
+    update_time = DateTimeField(auto_now=True, auto_now_add=False, null=True, blank=True, verbose_name=_('Updated'))
+
+    class Meta:
+        verbose_name = _('Management')
+        verbose_name_plural = _('Management')
+
+    def __str__(self):
+        return str(self.basefarmer)
+
+    def __unicode__(self):
+        return str(self.basefarmer)
+
+
+class FarmRelatedBusiness(Model):
+    name = CharField(max_length=50, null=True, blank=True, verbose_name=_('Related Business Name'))
+    has_business = CharField(max_length=1, null=True, blank=True, verbose_name=_('Has Business'))
+    update_time = DateTimeField(auto_now=True, auto_now_add=False, null=True, blank=True, verbose_name=_('Updated'))
+
+    class Meta:
+        verbose_name = _('FarmRelatedBusiness')
+        verbose_name_plural = _('FarmRelatedBusiness')
+
+    def __str__(self):
+        return str(self.name)
+
+    def __unicode__(self):
+        return str(self.name)
+    
+
+class Business(Model):
+    basefarmer = OneToOneField('basefarmers.BaseFarmer', related_name='business', verbose_name=_('BaseFarmer'))
+    farm_related_business = ManyToManyField('basefarmers.FarmRelatedBusiness', related_name='farm_related_business', verbose_name=_('Farm Related Business'))
+    update_time = DateTimeField(auto_now=True, auto_now_add=False, null=True, blank=True, verbose_name=_('Updated'))
+
+    class Meta:
+        verbose_name = _('Business')
+        verbose_name_plural = _('Business')
+
+    def __str__(self):
+        return str(self.basefarmer)
+
+    def __unicode__(self):
+        return str(self.basefarmer)
+
+
+class FarmerProductionType(Model):
+    name = CharField(max_length=20, null=True, blank=True, verbose_name=_('Production Type Name'))
+    update_time = DateTimeField(auto_now=True, auto_now_add=False, null=True, blank=True, verbose_name=_('Updated'))
+
+    class Meta:
+        verbose_name = _('FarmerProductionType')
+        verbose_name_plural = _('FarmerProductionType')
+
+    def __str__(self):
+        return str(self.name)
+
+    def __unicode__(self):
+        return str(self.name)
+
+
+class FarmerLandType(Model):
+    name = CharField(max_length=20, null=True, blank=True, verbose_name=_('Land Type Name'))
+    update_time = DateTimeField(auto_now=True, auto_now_add=False, null=True, blank=True, verbose_name=_('Updated'))
+
+    class Meta:
+        verbose_name = _('FarmerLandType')
+        verbose_name_plural = _('FarmerLandType')
+
+    def __str__(self):
+        return str(self.name)
+
+    def __unicode__(self):
+        return str(self.name)
+
+
+class FarmerLandArea(Model):
+    basefarmer = ForeignKey('basefarmers.BaseFarmer', related_name='farmer_land_areas', blank=True, verbose_name=_('BaseFarmer'))
+    farmer_production_type = ForeignKey('basefarmers.FarmerProductionType', related_name='farmer_production_type', verbose_name=_('Farmer Production Type'))
+    farmer_land_type = ForeignKey('basefarmers.FarmerLandType', related_name='farmer_land_type', verbose_name=_('Farmer Land Type'))
+    value = IntegerField(null=True, blank=True, verbose_name=_('Area Value'))
+    update_time = DateTimeField(auto_now=True, auto_now_add=False, null=True, blank=True, verbose_name=_('Updated'))
+
+    class Meta:
+        verbose_name = _('FarmerLandArea')
+        verbose_name_plural = _('FarmerLandArea')
+
+    def __str__(self):
+        return str(self.basefarmer)
+
+    def __unicode__(self):
+        return str(self.basefarmer)
+
+
+class Phone(Model):
+    basefarmer = ForeignKey('basefarmers.BaseFarmer', related_name='phones', verbose_name=_('BaseFarmer'))
+    phone = CharField(max_length=100, null=True, blank=True, verbose_name=_('Phone'))
+    update_time = DateTimeField(auto_now=True, auto_now_add=False, null=True, blank=True, verbose_name=_('Updated'))
+
+    class Meta:
+        verbose_name = _('Phone')
+        verbose_name_plural = _('Phone')
+
+    def __str__(self):
+        return str(self.basefarmer)
+
+    def __unicode__(self):
+        return str(self.basefarmer)
+
 class Lack(Model):
     #enough_worker = BooleanField(default=False, verbose_name=_('Enough Worker'))
     #uncertain_worker = BooleanField(default=False, verbose_name=_('Uncertain Worker'))
@@ -235,7 +254,6 @@ class Lack(Model):
     #is_lack = BooleanField(default=False, verbose_name=_('Is Lack'))
     name = CharField(max_length=50, unique=True, verbose_name=_('Name'))
     #is_lack = BooleanField(default=False, verbose_name=_('Is Lack'))
-
     update_time = DateTimeField(auto_now=True, auto_now_add=False, null=True, blank=True, verbose_name=_('Updated'))
 
     class Meta:
@@ -250,11 +268,10 @@ class Lack(Model):
 
 
 class AddressMatch(Model):
-    basefarmer = ForeignKey('basefarmers.BaseFarmer', related_name='basefarmer', verbose_name=_('BaseFarmer'))
+    basefarmer = OneToOneField('basefarmers.BaseFarmer', related_name='address_match', verbose_name=_('BaseFarmer'))
     match = BooleanField(default=False, verbose_name=_('Address Match'))
     different = BooleanField(default=False, verbose_name=_('Address Different'))
     address = CharField(max_length=100, null=True, blank=True, verbose_name=_('Address'))
-
     update_time = DateTimeField(auto_now=True, auto_now_add=False, null=True, blank=True, verbose_name=_('Updated'))
 
     class Meta:
@@ -301,7 +318,7 @@ class MarketType(Model):
 
 
 class AnnualIncome(Model):
-    #basefarmer = ForeignKey('basefarmers.BaseFarmer', null=True, blank=True, verbose_name=_('BaseFarmer'))
+    basefarmer = ForeignKey('basefarmers.BaseFarmer', related_name='annual_incomes', blank=True, verbose_name=_('BaseFarmer'))
     market_type = ForeignKey('basefarmers.MarketType', verbose_name=_('Market Type'))
     income_range_code = ForeignKey('basefarmers.IncomeRangeCode', verbose_name=_('Income Range Code'))
     update_time = DateTimeField(auto_now=True, auto_now_add=False, null=True, blank=True, verbose_name=_('Updated'))
@@ -311,9 +328,11 @@ class AnnualIncome(Model):
         verbose_name_plural = _('AnnualIncomes')
 
     def __str__(self):
-        return '%s(%s)' % (self.market_type, self.income_range_code)
+        return str(self.basefarmer)
+        #return '%s(%s)' % (self.market_type, self.income_range_code)
 
     def __unicode__(self):
-        return '%s(%s)' % (self.market_type, self.income_range_code)
+        return str(self.basefarmer)
+        #return '%s(%s)' % (self.market_type, self.income_range_code)
 
 
