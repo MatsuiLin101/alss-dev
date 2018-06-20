1610044eb05d270c71d75ecbe0387b81bbf12256
ssivart <travishen.tw@gmail.com>
2018-06-20 17:02:57 +0800

    modify loading promise

--- src/live-static-files/static-root/js/surveys18/index.js
+++ src/live-static-files/static-root/js/surveys18/index.js
@@ -64,7 +64,7 @@ $(document).ready(function() {
         Loading.open();
         setTimeout(function(){
             deferred.resolve();
-        }, 1000);
+        }, 500);
         return deferred.promise();
     }
 
