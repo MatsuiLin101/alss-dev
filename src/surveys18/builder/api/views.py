db6c0859ce0b819ce95c96514a853f2489181367
wups101 <35325179+wups101@users.noreply.github.com>
2018-06-21 15:05:32 +0800

    modify api view

--- src/surveys18/builder/api/views.py
+++ src/surveys18/builder/api/views.py
@@ -1,7 +1,7 @@
 from rest_framework.generics import (
     CreateAPIView,
 )
-from rest_framework.serializers import (
+from rest_framework.exceptions import (
     ValidationError,
 )
 from rest_framework.permissions import IsAdminUser
@@ -16,12 +16,15 @@ class BuilderFileCreateAPIView(CreateAPIView):
     # permission_classes = [IsAdminUser]
 
     def perform_create(self, serializer):
-        if serializer.is_valid():
-            datafile = self.request.data.get('datafile')
-            token = self.request.data.get('token')
-            user = self.request.user
-            if not isinstance(user, User):
-                user = None
+        try:
+            if serializer.is_valid():
+                datafile = self.request.data.get('datafile')
+                token = self.request.data.get('token')
+                user = self.request.user
+                if not isinstance(user, User):
+                    user = None
 
-            serializer.save(user=user,
-                            datafile=datafile, token=token)
+                serializer.save(user=user,
+                                datafile=datafile, token=token)
+        except Exception as e:
+            raise ValidationError(e)
