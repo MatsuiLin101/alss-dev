50b5a8f6bd3437c5d46176327aa2d4248bb33443
ssivart <travishen.tw@gmail.com>
2018-07-12 14:47:41 +0800

    add search_field to surveys18 admin

--- src/surveys18/admin.py
+++ src/surveys18/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from rangefilter.filter import DateRangeFilter
+from django.db.models import Q
 from .models import (
     BuilderFile,
     BuilderFileType,
@@ -52,8 +53,29 @@ class SurveyAdmin(admin.ModelAdmin):
                     'page',
                     'readonly',
                     'is_updated',
-                    'update_time')
-    list_filter = ('is_updated', 'readonly', 'page', ('update_time', DateRangeFilter))
+                    'update_time',)
+    list_filter = ('is_updated',
+                   'readonly',
+                   'page',
+                   ('update_time', DateRangeFilter),)
+    search_fields = (
+        'farmer_id',
+        'farmer_name'
+    )
+
+    def get_search_results(self, request, queryset, search_term):
+        queryset, use_distinct = super(SurveyAdmin, self).get_search_results(request, queryset, search_term)
+        try:
+            int(search_term)
+            queryset = self.model.objects.filter(
+                Q(farmer_id=search_term) |
+                Q(farmer_name=search_term) |
+                Q(id=search_term)
+            )
+        except ValueError:
+            queryset = self.model.objects.filter(farmer_name=search_term)
+
+        return queryset, use_distinct
 
 
 admin.site.register(Survey, SurveyAdmin)
