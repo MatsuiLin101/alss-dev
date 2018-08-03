9df094f379e0eb264f5f3a631e007fb173926570
MatsuiLin101 <matsuilin101@gmail.com>
2018-08-03 11:42:09 +0800

    create singleton api for reviewlog

--- src/logs/api/urls.py
+++ src/logs/api/urls.py
@@ -3,9 +3,11 @@ from django.conf.urls import url
 from .views import (
     ReviewLogViewSet,
     ReviewLogUpdateAPIView,
+    ReviewLogSingletonListAPIView,
 )
 
 urlpatterns = [
     url(r'^$', ReviewLogViewSet.as_view({'get': 'list'}), name='list'),
     url(r'^update/$', ReviewLogUpdateAPIView.as_view(), name='update'),
+    url(r'^reviewlog/$', ReviewLogSingletonListAPIView.as_view(), name='list_singleton_reviewlog'),
 ]
