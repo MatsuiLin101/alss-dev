2084736b88d031a5b7b36f61f5330e6cdf08d602
travishen.tw <travishen.tw@gmail.com>
2019-04-08 23:55:06 +0800

    Implements Travis CI

---
+++ src/config/settings/local.py
@@ -0,0 +1,11 @@
+from .base import *
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'postgres',
+        'PORT': '5432',
+    },
+}
